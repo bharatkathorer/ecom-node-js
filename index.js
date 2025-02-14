dc80817a9815580cce6b222a123901e1eb39ed6e
Bharat Kathore <49843578+bharatkathorer@users.noreply.github.com>
2025-02-14 11:32:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use('/', mainRoute);
 // for folder Access
 app.use('/public/storage', static('public/storage'))
 app.use('/storage', static('public/storage'))
-app.use('/tmp', static('tmp'))
+app.use('/tmp', static('/tmp/public/storage'))
 app.use('/', static('public'))
 // for folder Access
 
