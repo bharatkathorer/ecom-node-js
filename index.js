3a8f94ab350c466c3ac5d033a77831f68a6432f3
Bharat Kathore <49843578+bharatkathorer@users.noreply.github.com>
2025-02-18 20:16:52 +0530

    socket update

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ const {socketAuth} = require("./src/middleware/authMidlleware");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
+const allowedOrigins = ['https://ecom-react-seven-psi.vercel.app', 'http://localhost:5173'];
 // app.use(cors());
 app.use(cors({
-    origin: "*", // Allows all domains (change to specific domains in production)
+    origin: allowedOrigins, // Allows all domains (change to specific domains in production)
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
@@ -40,21 +40,13 @@ app.use('/', static('public'))
 
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
-   cors: {
-        origin: "*", // Allow all domains (change in production)
+    cors: {
+        origin: allowedOrigins,
         methods: ["GET", "POST"],
         allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true,
     },
-    handlePreflightRequest: (req, res) => {
-        res.writeHead(200, {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET,POST",
-            "Access-Control-Allow-Headers": "Content-Type, Authorization",
-            "Access-Control-Allow-Credentials": "true",
-        });
-        res.end();
-    },
+    path: '/socket.io'
 });
 io.use(socketAuth)
 chatController.initSocket(io);
